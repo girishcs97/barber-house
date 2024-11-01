3793271f88867579e1f1e39e7854f34ffd453dd0
girishcs97 <53255981+girishcs97@users.noreply.github.com>
2024-10-31 21:33:57 -0400

    Update URLconstants.js

--- src/URLconstants.js
+++ src/URLconstants.js
@@ -1,4 +1,4 @@
-export const DEFAULT_URL = 'http://localhost:8000';
+export const DEFAULT_URL = 'https://barbershopnode.onrender.com';
 export const SUBMIT_FORM = DEFAULT_URL+'/api/submitForm'
 export const GET_QUERIES = DEFAULT_URL+'/api/getqueries'
 export const SIGNIN_URL = DEFAULT_URL+'/access/signin'
